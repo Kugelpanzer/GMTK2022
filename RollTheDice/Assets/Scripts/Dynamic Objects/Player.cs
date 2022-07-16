bb393307da940c0805a88ce0abb4294f6c774459
Goran Damjanović <goran.goranche@gmail.com>
2022-07-16 14:01:20 +0200

    Strane kocke
    Vako moze da se utvrdi koja strana je gde...

--- RollTheDice/Assets/Scripts/Dynamic Objects/Player.cs
+++ RollTheDice/Assets/Scripts/Dynamic Objects/Player.cs
@@ -6,6 +6,90 @@ namespace GMTK2020
 {
 	public class Player : Critter
 	{
-		private PlayerDiceSide [] _sides = new PlayerDiceSide [6];
+		private enum SideOrientation { Up, Left, Right, Down }
+
+		private List<PlayerDiceSide> _sides = new List<PlayerDiceSide> ( 6 );
+		private int _sideUpIndex = 1;
+		private SideOrientation _sideOrientation = SideOrientation.Up;
+
+		public PlayerDiceSide GetSideUp ()
+		{
+			return _sides [_sideUpIndex];
+		}
+		public PlayerDiceSide GetSideDown ()
+		{
+			return _sides [5 - _sideUpIndex];
+		}
+		public PlayerDiceSide GetSideFront ()
+		{
+			return GetNextSide ( _sideUpIndex, _sideOrientation );
+		}
+		public PlayerDiceSide GetSideLeft ()
+		{
+			if ( _sideOrientation == SideOrientation.Up ) return GetNextSide ( _sideUpIndex, SideOrientation.Left );
+			if ( _sideOrientation == SideOrientation.Left ) return GetNextSide ( _sideUpIndex, SideOrientation.Down );
+			if ( _sideOrientation == SideOrientation.Right ) return GetNextSide ( _sideUpIndex, SideOrientation.Up );
+			if ( _sideOrientation == SideOrientation.Down ) return GetNextSide ( _sideUpIndex, SideOrientation.Right );
+			return null;
+		}
+		public PlayerDiceSide GetSideRight ()
+		{
+			if ( _sideOrientation == SideOrientation.Up ) return GetNextSide ( _sideUpIndex, SideOrientation.Right );
+			if ( _sideOrientation == SideOrientation.Left ) return GetNextSide ( _sideUpIndex, SideOrientation.Up );
+			if ( _sideOrientation == SideOrientation.Right ) return GetNextSide ( _sideUpIndex, SideOrientation.Down );
+			if ( _sideOrientation == SideOrientation.Down ) return GetNextSide ( _sideUpIndex, SideOrientation.Left );
+			return null;
+		}
+		public PlayerDiceSide GetSideBack ()
+		{
+			return GetNextSide ( _sideUpIndex, 3 - _sideOrientation );
+		}
+
+		private static PlayerDiceSide GetNextSide (int side, SideOrientation orientation)
+		{
+			if ( side == 1 )
+			{
+				if ( orientation == SideOrientation.Up ) return _sides [3];
+				if ( orientation == SideOrientation.Left ) return _sides [5];
+				if ( orientation == SideOrientation.Right ) return _sides [2];
+				if ( orientation == SideOrientation.Down ) return _sides [4];
+			}
+			if ( side == 2)
+			{
+				if ( orientation == SideOrientation.Up) return _sides [3];
+				if ( orientation == SideOrientation.Left) return _sides [1];
+				if ( orientation == SideOrientation.Right ) return _sides [6];
+				if ( orientation == SideOrientation.Down ) return _sides [4];
+			}
+			if ( side == 3 )
+			{
+				if ( orientation == SideOrientation.Up ) return _sides [6];
+				if ( orientation == SideOrientation.Left ) return _sides [5];
+				if ( orientation == SideOrientation.Right ) return _sides [2];
+				if ( orientation == SideOrientation.Down ) return _sides [1];
+			}
+			if ( side == 4 )
+			{
+				if ( orientation == SideOrientation.Up ) return _sides [1];
+				if ( orientation == SideOrientation.Left ) return _sides [5];
+				if ( orientation == SideOrientation.Right ) return _sides [2];
+				if ( orientation == SideOrientation.Down ) return _sides [6];
+			}
+			if ( side == 5 )
+			{
+				if ( orientation == SideOrientation.Up ) return _sides [3];
+				if ( orientation == SideOrientation.Left ) return _sides [6];
+				if ( orientation == SideOrientation.Right ) return _sides [1];
+				if ( orientation == SideOrientation.Down ) return _sides [4];
+			}
+			if ( side == 6 )
+			{
+				if ( orientation == SideOrientation.Up ) return _sides [4];
+				if ( orientation == SideOrientation.Left ) return _sides [5];
+				if ( orientation == SideOrientation.Right ) return _sides [2];
+				if ( orientation == SideOrientation.Down ) return _sides [3];
+			}
+			return null;
+		}
 	}
 }
