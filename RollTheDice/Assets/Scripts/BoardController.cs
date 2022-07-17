78aae76879b1ecda718d210ef0f92c4d63206b4c
Goran Damjanović <goran.goranche@gmail.com>
2022-07-17 11:27:18 +0200

    walls
    walls

--- RollTheDice/Assets/Scripts/BoardController.cs
+++ RollTheDice/Assets/Scripts/BoardController.cs
@@ -9,22 +9,32 @@ namespace GMTK2020
         public static BoardController Instance;
 
         public int GridSizeX, GridSizeY;
+        public List <Enemy> Enemies = new ();
+        public List <Wall> Walls = new ();
 
         private void Awake ()
         {
             if ( Instance == null ) Instance = this;
         }
 
-        // Start is called before the first frame update
-        void Start ()
-        {
+        public void ExecuteEnemiesTurn ()
+		{
+            foreach ( Enemy enemy in Enemies ) enemy.MoveOrAttack ();
+		}
 
-        }
+        public bool isOccupiedTile (int x, int y)
+		{
+            if ( x <= 0 || y <= 0 || x > GridSizeX || y > GridSizeY ) return false;
+            if (Player.Instance.GridPositionX == x && Player.Instance.GridPositionY == y) return true;
+            foreach ( Enemy enemy in Enemies ) if ( enemy.GridPositionX == x && enemy.GridPositionY == y ) return true;
+            foreach ( Wall wall in Walls ) if ( wall.GridPositionX == x && wall.GridPositionY == y ) return true;
+            return false;
+		}
+
+        // Start is called before the first frame update
+        void Start () {}
 
         // Update is called once per frame
-        void Update ()
-        {
-
-        }
+        void Update () {}
     }
 }
--- RollTheDice/Assets/Scripts/Dynamic Objects/Enemy.cs
+++ RollTheDice/Assets/Scripts/Dynamic Objects/Enemy.cs
@@ -6,16 +6,36 @@ namespace GMTK2020
 {
     public class Enemy : Critter
     {
+		public void MoveOrAttack ()
+		{
+			int directionX = Player.Instance.GridPositionX - GridPositionX;
+			int directionY = Player.Instance.GridPositionY - GridPositionY;
+			bool nextToPlayer = Mathf.Abs ( directionX ) + Mathf.Abs ( directionY ) <= 1;
+
+			if ( nextToPlayer ) AttackPlayer ();
+			else Move ();
+		}
+
 		// Move towards Player
 		public void Move ()
 		{
 			int directionX = Player.Instance.GridPositionX - GridPositionX;
 			int directionY = Player.Instance.GridPositionY - GridPositionY;
 
-			if ( directionY > Mathf.Abs ( directionX ) ) MoveUp ();
-			else if ( - directionY > Mathf.Abs ( directionX ) ) MoveDown ();
-			else if (directionX < 0) MoveLeft ();
-			else if (directionY > 0) MoveRight ();
+			bool canMoveLeft = !BoardController.Instance.isOccupiedTile ( GridPositionX - 1, GridPositionY );
+			bool canMoveRight = !BoardController.Instance.isOccupiedTile ( GridPositionX + 1, GridPositionY );
+			bool canMoveUp = !BoardController.Instance.isOccupiedTile ( GridPositionX, GridPositionY + 1 );
+			bool canMoveDown = !BoardController.Instance.isOccupiedTile ( GridPositionX, GridPositionY - 1 );
+
+			if ( canMoveUp && directionY > Mathf.Abs ( directionX ) ) MoveUp ();
+			else if ( canMoveDown && -directionY > Mathf.Abs ( directionX ) ) MoveDown ();
+			else if ( canMoveLeft && directionX < 0 ) MoveLeft ();
+			else if ( canMoveRight && directionY > 0 ) MoveRight ();
+		}
+
+		public void AttackPlayer ()
+		{
+			Player.Instance.DecreaseHealth ();
 		}
 	}
 }
